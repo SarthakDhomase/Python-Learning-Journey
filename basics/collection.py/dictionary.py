2b7953919791f0a917c1d6bc630c123d9c4124d0
Sarthak Dhomase <sarthakdhomase1976@gmail.com>
2026-08-18 15:16:13 +0530

    rename collection.py to collection

--- basics/collection.py/dictionary.py
+++
@@ -1,9 +0,0 @@
-# dictionary = a collection of {key:value} pairs
-#               ordered and changeable. No duplicates
-
-capitals = {"India":"New Delhi",
-            "USA":"Washington D.C",
-            "China":"Beiging",
-            "Russia":"Moscow"}
-
-print(capitals.get("USA")) #to get any form the dictionary
